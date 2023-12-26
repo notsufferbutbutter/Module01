40da6a70caf3de2a77ef28e3e44f4e3501f9d500
notsufferbutbutter <143406531+notsufferbutbutter@users.noreply.github.com>
2023-12-26 13:54:24 +0100

    Add files via upload

---
+++ Algorithm_8.java
@@ -0,0 +1,22 @@
+class Algorithm_8 {
+
+    //cau 4
+    public static void printAsFunction(String x) {
+        System.out.println(x);
+    }
+
+    public static void main (String[]args) {
+        //cau 1
+        System.out.println("hello world");
+        //cau 2
+        String helloWorld = "hello world"; 
+        System.out.println(helloWorld);
+        //cau 3
+        String hello = "hello";
+        String world = "world";
+        String X = hello + " " + world;
+        System.out.println(X);
+        //cau 4
+        printAsFunction(X);   
+    }
+}
